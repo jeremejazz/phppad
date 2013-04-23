f22a3a77fe4abc381b9b4f77f4826438bb30201c
Jereme <jeremejazz15@yahoo.com>
2013-04-23 12:40:00 +0800

    Removed magic quotes error

--- phppad.php
+++ phppad.php
@@ -1,7 +1,23 @@
 <?php
 /*PHP Pad*/
 
-/*Turn off magic quotes before using */
+/*Disable magic quotes if turned on */
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
 
 ?>
 
@@ -13,54 +29,11 @@
 	<script src="codemirror/lib/codemirror.js"></script>
 <link rel="stylesheet" href="codemirror/lib/codemirror.css">
 <link rel="stylesheet" href="codemirror/theme/monokai.css">
+<link rel="stylesheet" href="css/styles.css">
     <script src="codemirror/mode/clike/clike.js"></script>
 <script src="codemirror/mode/php/php.js"></script>
 
-	<style type="text/css">
-		.warning{
-			border: dotted 1px red ;
-			background-color: yellow;
-			color:red;
-		}
-		#output{
-			border: dotted 1px black;
-			min-height: 100px;
-			padding:10px;
-		}
-
-		.button { 
-     border-top: 1px solid #96d1f8;
-     background: #000896;
-     background: -webkit-gradient(linear, left top, left bottom, from(#004be0), to(#000896));
-     background: -moz-linear-gradient(top, #004be0, #000896);
-     padding: 8px 16px;
-     -webkit-border-radius: 11px;
-     -moz-border-radius: 11px;
-     border-radius: 11px;
-     -webkit-box-shadow: rgba(0,0,0,1) 0 1px 0;
-     -moz-box-shadow: rgba(0,0,0,1) 0 1px 0;
-     box-shadow: rgba(0,0,0,1) 0 1px 0;
-     text-shadow: rgba(0,0,0,.4) 0 1px 0;
-     color: white;
-     font-size: 13px;
-     font-family: 'Lucida Sans Unicode', 'Lucida Grande', sans-serif;
-     text-decoration: none;
-     vertical-align: middle;
-     cursor:pointer;
-  }
-  .button:hover { 
-     border-top-color: #000ea6;
-     background: #000ea6;
-     color: #ccc;
-  }
-  .button:active { 
-     border-top-color: #1b435e;
-     background: #1b435e;
-  }
-  .bottomtools{
-  	text-align: right;
-  }
-	</style>
+	
 
 </head>
 <body>
