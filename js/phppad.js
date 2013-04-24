daf95d9b0a1d68769fea6d1d408142218ef0490a
Jereme <jeremejazz15@yahoo.com>
2013-04-24 16:24:57 +0800

    Use ajax for submit

---
+++ js/phppad.js
@@ -0,0 +1,32 @@
+
+	window.onkeydown = function(e){
+  if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
+    e.preventDefault();
+    //your saving code
+    alert('Unfortunately You cannot Save');
+  }
+
+
+}
+
+	var myTextArea = document.getElementById('codearea');
+var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+	    lineNumbers: true,
+		  matchBrackets: true,
+        mode: "application/x-httpd-php",
+        indentUnit: 4,
+        indentWithTabs: true,
+        enterMode: "keep",
+        tabMode: "shift",
+        theme: "monokai"
+
+});
+
+
+    $("#padform").submit(function(){
+
+        $("#output").load('pad.php',
+                { code: $("#codearea").val() }).hide().fadeIn();
+
+        return false;
+    });
--- phppad.php
+++ phppad.php
@@ -1,6 +1,7 @@
 <?php
 /*PHP Pad*/
 
+
 /*Disable magic quotes if turned on */
 if (get_magic_quotes_gpc()) {
     $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
@@ -18,7 +19,6 @@ if (get_magic_quotes_gpc()) {
     unset($process);
 }
 
-
 ?>
 
 <!DOCTYPE html />
@@ -31,8 +31,9 @@ if (get_magic_quotes_gpc()) {
 <link rel="stylesheet" href="codemirror/lib/codemirror.css">
 <link rel="stylesheet" href="codemirror/theme/monokai.css">
 <link rel="stylesheet" href="css/styles.css">
-    <script src="codemirror/mode/clike/clike.js"></script>
+<script src="codemirror/mode/clike/clike.js"></script>
 <script src="codemirror/mode/php/php.js"></script>
+<script src="js/jquery-1.9.1.min.js"></script>
 
 	
 
@@ -51,7 +52,7 @@ if (get_magic_quotes_gpc()) {
         <?php
     }
         ?>
-    	<form id="myform" method="POST" action = "">
+    	<form id="padform" method="POST" action = "">
 
     		<textarea id="codearea" name="code" rows="10"><?php echo $_REQUEST['code']; ?></textarea> <br/>
     		<div class="bottomtools"><input type="submit" name="submit" value="submit" class="button" /></div>
@@ -61,40 +62,14 @@ if (get_magic_quotes_gpc()) {
     	<div id="output">
     		<?php 
 
-    			if($_REQUEST['submit']){
-    				eval( '?>' . $_REQUEST['code']);
+                if($_REQUEST['submit']){
+                eval( '?>' . $_REQUEST['code']);
 
-    			}
+                }
 
     		?>
     	</div>
 </div>
-	<script type="text/javascript">
-
-	window.onkeydown = function(e){
-  if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
-    e.preventDefault();
-    //your saving code
-    alert('Unfortunately You cannot Save');
-  }
-
-
-}
-
-	var myTextArea = document.getElementById('codearea');
-var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
-	    lineNumbers: true,
-		  matchBrackets: true,
-        mode: "application/x-httpd-php",
-        indentUnit: 4,
-        indentWithTabs: true,
-        enterMode: "keep",
-        tabMode: "shift",
-        theme: "monokai"
-
-});
-
-
-	</script>
+	<script type="text/javascript" src="js/phppad.js"></script>
 </body>
 </html>
